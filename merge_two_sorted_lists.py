22a9de92b0f67033b3203607e50707dcfac68425
cauverig <79813817+cauverig@users.noreply.github.com>
2022-01-01 22:03:21 -0800

    Update merge_two_sorted_lists.py

--- merge_two_sorted_lists.py
+++ merge_two_sorted_lists.py
@@ -1,3 +1,10 @@
+"""You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list."""
+
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         Starter = ListNode(0)  # Sentinel node
