f37fc82119b8eef47a3cad4454adfd9638097294
cauverig <cauverig@gmail.com>
2022-01-21 23:29:11 -0800

    add binary

---
+++ add_binary.py
@@ -0,0 +1,27 @@
+"""Given two binary strings a and b, return their sum as a binary string.
+
+Constraints:
+1 <= a.length, b.length <= 104
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.
+"""
+
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        carry = 0
+        result = ''
+
+        a = list(a)
+        b = list(b)
+
+        while a or b or carry:
+            if a:
+                carry += int(a.pop())
+            if b:
+                carry += int(b.pop())
+
+            result += str(carry %2)
+            carry //= 2
+
+        return result[::-1]
